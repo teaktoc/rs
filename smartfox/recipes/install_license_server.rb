63a81a0b7e53a127a2685dfb8aa4862d57d15071
Stefhen Hovland <stefhen@rightscale.com>
2013-11-08 16:01:53 -0500

    Changed to .tar.gz

--- smartfox/recipes/install_license_server.rb
+++ smartfox/recipes/install_license_server.rb
@@ -4,13 +4,13 @@ bash "download_smartfox" do
   code <<-EOM
     export STORAGE_ACCOUNT_ID="#{node[:smartfox][:storage_account_id]}"
     export STORAGE_ACCOUNT_SECRET="#{node[:smartfox][:storage_account_secret]}"
-    /opt/rightscale/sandbox/bin/ros_util get -c "#{node[:smartfox][:bucket]}" -s "#{node[:smartfox][:file]}" -C "#{node[:smartfox][:provider]}" -d "#{Chef::Config[:file_cache_path]}/smartfox.tar.bz2"
+    /opt/rightscale/sandbox/bin/ros_util get -c "#{node[:smartfox][:bucket]}" -s "#{node[:smartfox][:file]}" -C "#{node[:smartfox][:provider]}" -d "#{Chef::Config[:file_cache_path]}/smartfox.tar.gz"
   EOM
 end
 
 bash "untar_smartfox" do
   code <<-EOM
-   tar jxvf "#{Chef::Config[:file_cache_path]}/smartfox.tar.bz2" -C /usr/local
+   tar zxvf "#{Chef::Config[:file_cache_path]}/smartfox.tar.gz" -C /usr/local
    ln -s /usr/local/SFS_PRO_1.6.6 /usr/local/smartfox
  EOM
 end
