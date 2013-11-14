a95f1a3d50e88926caa3c3365d7302c7d19365ad
Stefhen Hovland <stefhen@rightscale.com>
2013-11-14 17:04:09 -0500

    Updated metadata

--- smartfox/metadata.rb
+++ smartfox/metadata.rb
@@ -10,6 +10,7 @@ version          '0.1.0'
 depends "rightscale"
 
 recipe "smartfox::install_license_server", "Installs and configures SmartFox Pro License Server"
+recipe "smartfox::install_client", "Install SmartFox game server client"
 
 attribute "smartfox/storage_account_id",
   :description => "API public key of storage provider (s3, cloudfiles, ...)",
@@ -37,4 +38,18 @@ attribute "smartfox/file",
   :recipes     => ["smartfox::install", "smartfox::install_license_server"],
   :type        => "string",
   :display     => "smartfox/bucket",
+  :required    => "required"
+
+attribute "smartfox/license_server",
+  :description => "DNS address of Smartfox License Server",
+  :recipes     => ["smartfox::install_client"],
+  :type        => "string",
+  :display     => "smartfox/license_server",
+  :required    => "required"
+
+attribute "smartfox/license_number",
+  :description => "License slot number",
+  :recipes     => ["smartfox::install_client"],
+  :type        => "string",
+  :display     => "smartfox/license_server",
   :required    => "required"
